ab951202b9376176f3c76697c5695f250d6c98f3
JArmandoBL <armand.jo.9712@gmail.com>
2022-09-06 19:20:15 -0400

    primer commit crud rest 06092022

---
+++ src/main/java/py/edu/facitec/spring_rest_2Application.java
@@ -0,0 +1,13 @@
+package py.edu.facitec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class spring_rest_2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(spring_rest_2Application.class, args);
+	}
+
+}
